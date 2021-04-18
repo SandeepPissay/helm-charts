ed1bb62a918dbee86351cb8ac096f1db82db9075
Sandeep Pissay Srinivasa Rao <ssrinivas@vmware.com>
2021-04-18 13:43:03 -0700

    Minor changes to create and delete secrets scripts.

--- elasticsearch/examples/security/create_secrets.sh
+++ elasticsearch/examples/security/create_secrets.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 if [ $# -ne 1 ]
 then
-	echo "create_secrets.sh <kube_config_file>"
+	echo "create_secrets.sh <namespace>"
 	echo "Number of parameters: $#, expected 1. Exiting !!!"
 	exit -1
 fi
-export KUBECONFIG=$1
+export NAMESPACE=$1
 password=`< /dev/urandom tr -cd '[:alnum:]' | head -c20`
-kubectl -n observability create secret generic elastic-certificates --from-file=elastic-certificates.p12 && \
-       kubectl -n observability create secret generic elastic-certificate-pem --from-file=elastic-certificate.pem && \
-       kubectl -n observability create secret generic elastic-certificate-crt --from-file=elastic-certificate.crt && \
-       kubectl -n observability create secret generic elastic-credentials --from-literal=password=$password --from-literal=username=elastic
+kubectl -n $NAMESPACE create secret generic elastic-certificates --from-file=elastic-certificates.p12 && \
+       kubectl -n $NAMESPACE create secret generic elastic-certificate-pem --from-file=elastic-certificate.pem && \
+       kubectl -n $NAMESPACE create secret generic elastic-certificate-crt --from-file=elastic-certificate.crt && \
+       kubectl -n $NAMESPACE create secret generic elastic-credentials --from-literal=password=$password --from-literal=username=elastic
--- elasticsearch/examples/security/delete_secrets.sh
+++ elasticsearch/examples/security/delete_secrets.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 if [ $# -ne 1 ]
 then
-	echo "create_secrets.sh <kube_config_file>"
+	echo "delete_secrets.sh <namespace>"
 	echo "Number of parameters: $#, expected 1. Exiting !!!"
 	exit -1
 fi
-export KUBECONFIG=$1
-kubectl -n observability delete secrets elastic-credentials elastic-certificates elastic-certificate-pem elastic-certificate-crt|| true
+export NAMESPACE=$1
+kubectl -n $NAMESPACE delete secrets elastic-credentials elastic-certificates elastic-certificate-pem elastic-certificate-crt|| true
